26df17a1e596452d22be1324d2c25402b136a52c
Aditya-Prakash14 <ap312038@gmail.com>
2026-02-09 18:07:31 +0530

    all test

--- tests/example.test.js
+++ tests/example.test.js
@@ -8,4 +8,29 @@ test("Check for invalid sub total", () => {
   expect(() => calculateFinalAmount(-100, "DISCOUNT")).toThrow(
     "Invalid subtotal",
   );
+     });
+
+
+test("Error on negative subtotal", () => {
+  expect(() => calculateFinalAmount(-50, null)).toThrow("Invalid subtotal");
+ });
+ test("No coupon", () => {
+  const result = calculateFinalAmount(200, null);
+  expect(result).toEqual(200); 
+ });
+   test("SAVE10", () => {
+  const result = calculateFinalAmount(100, "SAVE10");
+  expect(result).toEqual(90);
+ });
+   test("FLAT50", () => {
+  const result = calculateFinalAmount(50, "FLAT50");
+  expect(result).toEqual(0);
 });
+test("Work coupon is lowercase", () => {
+  const result = calculateFinalAmount(100, "flat50");
+  expect(result).toEqual(50);
+    });
+  test("Automatic discount on large orders", () => {
+  const result = calculateFinalAmount(1000, null);
+  expect(result).toEqual(950);
+  });
